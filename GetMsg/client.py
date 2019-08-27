29f65eddf53acc8af7456cce8cb443d51f8c3f63
whn24 <1142353214@qq.com>
2019-08-27 16:04:05 +0800

    客户端通信

---
+++ GetMsg/client.py
@@ -0,0 +1,53 @@
+import socket
+import time
+import sys
+
+HOST = '139.217.133.111'
+PORT = 8080
+BUFIZ = 1024
+ADDR = (HOST, PORT)
+
+class Client:
+    def __init__(self):
+        self.cliSock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.cliSock.connect(ADDR)
+
+    def sendUsr(self, usr):
+        t = 'a' + usr
+        self.cliSock.send(bytes(t, encoding='utf-8'))
+        time.sleep(0.2)
+
+
+
+    def sendemail(self, text):
+        t = 'b' + text
+        self.cliSock.send(bytes(t, encoding='utf-8'))
+        time.sleep(0.2)
+
+
+    def sendBlack(self, name):
+        t = 'c' + name
+        self.cliSock.send(bytes(t, encoding='utf-8'))
+        time.sleep(0.2)
+
+
+    def sendWhite(self, name):
+        t = 'd' + name
+        self.cliSock.send(bytes(t, encoding='utf-8'))
+        time.sleep(0.2)
+
+
+    def getresult(self):
+        msg = self.cliSock.recv(4096)
+        t = str(msg, encoding='utf-8')
+        print(t)
+        return t
+
+    def close(self):
+        self.cliSock.close()
+
+if __name__ == "__main__":
+        cli = Client()
+        cli.sendemail('mail')
+        cli.getresult()
+        cli.close()
